bae46abe0101641e7250b4e0e18eb821bf456cfc
tatsuya1995 <tatsuyawada1995@gmail.com>
2023-07-18 22:00:15 +0900

    feature/t.wada/FUKUOKA_DEV-5 - VPC->PublicSubnetの実装

--- network.sh
+++ network.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-## https://zenn.dev/amarelo_n24/articles/35cb14a057ecf1
 
 # VPC
 aws ec2 create-vpc \
@@ -20,8 +19,26 @@ aws ec2 attach-internet-gateway \
    --vpc-id $VPC_ID
 
 # Public Subnet
-
-# Private Subnet
+PUBLIC_SUBNET_ID_1A=$(aws ec2 create-subnet \
+    --vpc-id $VPC_ID \
+    --cidr-block 10.0.12.0/24 \
+    --availability-zone ap-northeast-1a \
+    --tag-specifications "ResourceType=subnet, Tags=[{Key=Name, Value=fukuoka-public-subnet-1a}]" \
+    --query "Subnet.SubnetId" --output text)
 
 # Root Table
+ROUTE_TABLE_ID=$(aws ec2 create-route-table \
+    --vpc-id $VPC_ID \
+    --tag-specifications "ResourceType=route-table, Tags=[{Key=Name, Value=fukuoka-public-route}]" \
+    --query "RouteTable.RouteTableId" --output text)
 
+# Root Table TO IGW
+aws ec2 create-route \
+    --route-table-id $ROUTE_TABLE_ID \
+    --destination-cidr-block 0.0.0.0/0 \
+    --gateway-id $INTERNET_GATEWAY_ID
+
+# Root Table TO Public Subnet
+aws ec2 associate-route-table \
+    --route-table-id $ROUTE_TABLE_ID \
+    --subnet-id $PUBLIC_SUBNET_ID_1A
